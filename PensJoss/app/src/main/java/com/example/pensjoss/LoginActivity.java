70bc9c4c2be407ce09f9b684a6951ac3bcbac519
pringgojs <odyinggo@gmail.com>
2019-07-15 18:56:22 +0700

    fix layout

--- PensJoss/app/src/main/java/com/example/pensjoss/LoginActivity.java
+++ PensJoss/app/src/main/java/com/example/pensjoss/LoginActivity.java
@@ -2,13 +2,57 @@ package com.example.pensjoss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static com.example.pensjoss.SignupActivity.isValidEmail;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText edUsername, edPassword;
+    Button btLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edUsername = findViewById(R.id.edUsername);
+        edPassword = findViewById(R.id.edPassword);
+    }
+
+    public void login(View view) {
+        if (TextUtils.isEmpty(edUsername.getText().toString().trim())
+                && TextUtils.isEmpty(edPassword.getText().toString().trim())) {
+            edUsername.setError("Email tidak boleh kosong!");
+            edPassword.setError("Password tidak boleh kosong!");
+
+        } else if (!isValidEmail(edUsername.getText().toString().trim())) {
+
+            edUsername.setError("Email tidak valid!");
+
+        } else if (TextUtils.isEmpty(edUsername.getText().toString().trim())) {
+
+            edUsername.setError("Email tidak boleh kosong!");
+
+        } else if (TextUtils.isEmpty(edPassword.getText().toString())) {
+            edPassword.setError("Password tidak boleh kosong!");
+        } else {
+            Toast.makeText(view.getContext(), "Sukses Login", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void register(View view) {
+        Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(i);
+    }
+
+    public static boolean isValidEmail(CharSequence email) {
+        return (Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 }
--- PensJoss/app/src/main/java/com/example/pensjoss/SignupActivity.java
+++ PensJoss/app/src/main/java/com/example/pensjoss/SignupActivity.java
@@ -3,12 +3,56 @@ package com.example.pensjoss;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
+    EditText edFullname, edEmail, edPassword, edRepassword, edPhone, edBirtday;
+    RadioButton rdMale, rdFamale;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        edFullname = findViewById(R.id.edFullname);
+        edEmail = findViewById(R.id.edEmail);
+        edPassword = findViewById(R.id.edPassword);
+        edRepassword = findViewById(R.id.edRepassword);
+        edPhone = findViewById(R.id.edPhone);
+        edBirtday = findViewById(R.id.edBirthday);
+        rdMale = findViewById(R.id.rdMale);
+        rdFamale = findViewById(R.id.rdFamale);
+    }
+
+    public void signUp(View view) {
+        String password = edPassword.getText().toString().trim();
+        String rePassword = edRepassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(edFullname.getText().toString().trim())) {
+            edFullname.setError("Fullname can't be empty!");
+        } else if (!isValidEmail(edEmail.getText().toString().trim())) {
+            edEmail.setError("Email not valid!");
+        } else if (TextUtils.isEmpty(password)) {
+            edPassword.setError("Password can't be empty!");
+        } else if (!rePassword.equals(password)) {
+            edRepassword.setError("Password not same!");
+        } else if (TextUtils.isEmpty(edPhone.getText().toString().trim())) {
+            edPhone.setError("Phone can't be empty!");
+        } else if (TextUtils.isEmpty(edBirtday.getText().toString().trim())) {
+            edBirtday.setError("Birtday can't be empty!");
+        } else if (rdMale.isChecked() || rdFamale.isChecked()) {
+            Toast.makeText(view.getContext(), "Gender not selected", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(view.getContext(), "Register complate", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static boolean isValidEmail(CharSequence email) {
+        return (Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 }
